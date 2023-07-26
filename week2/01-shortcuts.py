1c56ae88a5262e607c14f7a4600d60e493401f28
suyeon <stgood@kookmin.ac.kr>
2023-07-26 19:11:00 +0900

    07/26

--- week2/01-shortcuts.py
+++ week2/01-shortcuts.py
@@ -1,3 +1,26 @@
+# import requests
+# from bs4 import BeautifulSoup
+
+# r = requests.get('https://overstudy.com/parathon')
+
+# soup = BeautifulSoup(r.text, 'html.parser')
+
+# print('soup.title')
+# # the first matched tag
+# print('tag:  ', soup.title)
+
+# # name (tag name)
+# print('name: ', soup.title.name)
+
+# # attrubutes
+# print('attrs:', soup.title.attrs)
+
+# print('---------')
+# print('soup.div.div.div')
+# print('tag:  ', soup.div.div.div)
+# print('name: ', soup.div.div.div.name)
+# print('attrs:', soup.div.div.div.attrs)
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,18 +28,16 @@ r = requests.get('https://overstudy.com/parathon')
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
+soup.title.string = "Parathon"
+soup.title.name = 'title'
 print('soup.title')
-# the first matched tag
-print('tag:  ', soup.title)
 
-# name (tag name)
+print('tag: ', soup.title)
 print('name: ', soup.title.name)
+print('attributes : ', soup.title.attrs)
 
-# attrubutes
-print('attrs:', soup.title.attrs)
-
-print('---------')
+print("---------------")
 print('soup.div.div.div')
-print('tag:  ', soup.div.div.div)
+print('tag: ', soup.div.div.div)
 print('name: ', soup.div.div.div.name)
-print('attrs:', soup.div.div.div.attrs)
+print('attrs: ', soup.div.div.div.attrs)
